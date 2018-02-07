1c68c844b525aad271aaeb22d310635a79f4924a
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-08 00:21:08 +0500

    Exceptions

---
+++ src/Exception/FluentTableException.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of Comely Fluent ORM package.
+ * https://github.com/comelyio/fluent
+ *
+ * Copyright (c) 2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comelyio/fluent/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\Fluent\Exception;
+
+/**
+ * Class FluentTableException
+ * @package Comely\Fluent\Exception
+ */
+class FluentTableException extends FluentException
+{
+    /**
+     * @param string $col
+     * @param string $message
+     * @return FluentTableException
+     */
+    public static function ColumnError(string $col, string $message): self
+    {
+        return new self(sprintf('%s for column "%s"', $message, $col));
+    }
+}
