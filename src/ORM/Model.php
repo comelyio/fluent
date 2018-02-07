035d058e25108315a1b88a47218285eeaf42890d
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-08 00:21:20 +0500

    Model init

---
+++ src/ORM/Model.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of Comely Fluent ORM package.
+ * https://github.com/comelyio/fluent
+ *
+ * Copyright (c) 2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comelyio/fluent/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\Fluent\ORM;
+
+/**
+ * Class Model
+ * @package Comely\Fluent\ORM
+ */
+abstract class Model
+{
+}
