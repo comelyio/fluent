b2f9e44a69386b239d7a521eab06daadd15b1dc8
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-08 00:47:44 +0500

    Precision columns trait

---
+++ src/Database/Table/Traits/PrecisionColumnTrait.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * This file is part of Comely Fluent ORM package.
+ * https://github.com/comelyio/fluent
+ *
+ * Copyright (c) 2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comelyio/fluent/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\Fluent\Database\Table\Traits;
+
+use Comely\Fluent\Exception\FluentTableException;
+use Comely\Kernel\Toolkit\Number;
+
+/**
+ * Trait PrecisionColumnTrait
+ * @package Comely\Fluent\Database\Table\Traits
+ */
+trait PrecisionColumnTrait
+{
+    /**
+     * @param int $digits
+     * @param int $scale
+     * @return self
+     * @throws FluentTableException
+     */
+    public function precision(int $digits, int $scale): self
+    {
+        // Precision digits
+        if (!Number::Range($digits, 1, self::PRECISION_MAX)) {
+            throw FluentTableException::ColumnError(
+                $this->name,
+                sprintf('Precision digits must be between 1 and %d', self::PRECISION_MAX)
+            );
+        }
+
+        // Scale
+        $maxScale = $digits;
+        if ($maxScale > self::SCALE_MAX) {
+            $maxScale = self::SCALE_MAX;
+        }
+
+        if (!Number::Range($scale, 0, $maxScale)) {
+            throw FluentTableException::ColumnError(
+                $this->name,
+                sprintf('Scale digits must be between 0 and %d', $maxScale)
+            );
+        }
+
+
+        // Set
+        $this->digits = $digits;
+        $this->scale = $scale;
+        return $this;
+    }
+}
