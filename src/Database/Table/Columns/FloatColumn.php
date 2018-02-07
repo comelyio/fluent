7c4d93b7c1765bd50ddd5ae4325586476d9e96bd
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-08 00:54:39 +0500

    Float Column

---
+++ src/Database/Table/Columns/FloatColumn.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * This file is part of Comely Fluent ORM package.
+ * https://github.com/comelyio/fluent
+ *
+ * Copyright (c) 2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comelyio/fluent/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\Fluent\Database\Table\Columns;
+
+use Comely\Fluent\Database\Table\Traits\NullableColumnTrait;
+use Comely\Fluent\Database\Table\Traits\NumericColumnTrait;
+use Comely\Fluent\Database\Table\Traits\PrecisionColumnTrait;
+use Comely\Fluent\Exception\FluentTableException;
+
+/**
+ * Class FloatColumn
+ * @package Comely\Fluent\Database\Table\Columns
+ */
+class FloatColumn extends AbstractColumn
+{
+    const PRECISION_MAX = 65;
+    const SCALE_MAX = 30;
+
+    /** @var int */
+    protected $digits;
+    /** @var int */
+    protected $scale;
+
+    use NullableColumnTrait;
+    use NumericColumnTrait;
+    use PrecisionColumnTrait;
+
+    /**
+     * DecimalColumn constructor.
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        parent::__construct($name);
+        $this->type = "float";
+        $this->scalarType = "double";
+        $this->digits = 10;
+        $this->scale = 0;
+        $this->default = 0;
+    }
+
+    /**
+     * @param float|null $value
+     * @return FloatColumn
+     * @throws FluentTableException
+     */
+    public function default(?float $value = 0): self
+    {
+        $this->setDefaultValue($value);
+        return $this;
+    }
+}
