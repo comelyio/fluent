5a07efc94d4c2a680dfa84f4bf6650574ab03ec3
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-09 00:32:13 +0500

    Prototypes

--- prototype/SampleTable.php
+++ prototype/SampleTable.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
 
 namespace App\Database;
 
-use Comely\Fluent\Database\Table;
+use Comely\Fluent\Database\FluentTable;
 use Comely\Fluent\Database\Table\Columns;
 use Comely\Fluent\Database\Table\Constraints;
 
@@ -22,8 +22,11 @@ use Comely\Fluent\Database\Table\Constraints;
  * Class SampleTable
  * @package App\Database
  */
-class SampleTable extends Table
+class SampleTable extends FluentTable
 {
+    const NAME = 'sample_table';
+    const MODEL = 'App\Models\SampleModel';
+
     /**
      * @param Columns $cols
      * @param Constraints|null $constraints
@@ -33,7 +36,7 @@ class SampleTable extends Table
     {
         $cols->int("id")->size(self::INT_MEDIUM)->unSigned()->autoIncrement();
         $cols->int("status")->size(self::INT_TINY, 1)->unSigned()->default(0);
-        $cols->enum("role")->options("user","mod")->default("user");
+        $cols->enum("role")->options("user", "mod")->default("user");
         $cols->string("email")->length(255)->unique();
         $cols->string("password")->fixed(40)->nullable();
         $cols->binary("token")->fixed(10)->nullable();
