ed68ab50d5870ba6b8032264effba8d32e2107ca
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-09 22:15:18 +0500

    Fluent

--- src/Fluent.php
+++ src/Fluent.php
@@ -14,10 +14,58 @@ declare(strict_types=1);
 
 namespace Comely\Fluent;
 
+use Comely\Fluent\Database\Table;
+use Comely\Fluent\Exception\FluentException;
+use Comely\IO\Database\Database;
+
 /**
  * Class Fluent
  * @package Comely\Fluent
  */
 class Fluent
 {
+    /** @var array */
+    private static $tables = [];
+    /** @var array */
+    private static $index = [];
+
+    /**
+     * @param Database $db
+     * @param string $table
+     * @throws FluentException
+     */
+    public static function Bind(Database $db, string $table): void
+    {
+        if (!class_exists($table)) {
+            throw new FluentException(sprintf('FluentTable class "%s" not found', $table));
+        } elseif (!is_subclass_of($table, 'Comely\Fluent\Database\Table', true)) {
+            throw new FluentException(sprintf('Class "%s" is not FluentTable', $table));
+        }
+
+        self::Append(new $table($db));
+    }
+
+    /**
+     * @param Table $table
+     */
+    public static function Append(Table $table): void
+    {
+        self::$tables[$table->_name] = $table;
+        self::$index[get_class($table)] = $table->_name;
+    }
+
+    /**
+     * @param string $name
+     * @return Table
+     * @throws FluentException
+     */
+    public static function Retrieve(string $name): Table
+    {
+        $table = self::$tables[$name] ?? self::$tables[self::$index[$name] ?? ""] ?? null;
+        if (!$table) {
+            throw new FluentException(sprintf('FluentTable "%s" not found', $name));
+        }
+
+        return $table;
+    }
 }
