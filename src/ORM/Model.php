0ea86cac9a250fea8153c05225ea01987c253bd5
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-25 12:15:19 +0500

    Prefix all props with "_"

--- src/ORM/Model.php
+++ src/ORM/Model.php
@@ -41,15 +41,15 @@ abstract class Model implements \Serializable
     public const SERIALIZABLE = false;
 
     /** @var string */
-    private $name;
+    private $_name;
     /** @var \Comely\Fluent\Database\Table */
-    private $table;
+    private $_table;
     /** @var array */
-    private $privateProps;
+    private $_privateProps;
     /** @var array */
-    private $original;
+    private $_original;
     /** @var null|AbstractColumn */
-    private $primaryColumn;
+    private $_primaryColumn;
 
     /**
      * Model constructor.
@@ -58,15 +58,15 @@ abstract class Model implements \Serializable
      */
     final public function __construct(array $row = null)
     {
-        $this->name = get_called_class();
-        $this->privateProps = [];
-        $this->original = [];
+        $this->_name = get_called_class();
+        $this->_privateProps = [];
+        $this->_original = [];
 
         // Check TABLE constant
         $table = static::TABLE;
         if (!is_string($table) || !preg_match('/^[a-zA-Z0-9\_\\\]+$/', $table)) {
             throw new FluentModelException(
-                sprintf('Invalid value for TABLE constant in FluentModel class "%s"', $this->name)
+                sprintf('Invalid value for TABLE constant in FluentModel class "%s"', $this->_name)
             );
         }
 
@@ -92,7 +92,7 @@ abstract class Model implements \Serializable
         $serializable = static::SERIALIZABLE;
         if (!is_bool($serializable)) {
             throw new FluentModelException(
-                sprintf('constant SERIALIZABLE declared in "%s" must be of type boolean', $this->name)
+                sprintf('constant SERIALIZABLE declared in "%s" must be of type boolean', $this->_name)
             );
         }
 
@@ -106,7 +106,7 @@ abstract class Model implements \Serializable
     final private function loadTable(string $table): void
     {
         try {
-            $this->table = Fluent::Retrieve($table);
+            $this->_table = Fluent::Retrieve($table);
         } catch (FluentException $e) {
             throw new FluentModelException($e->getMessage());
         }
@@ -121,7 +121,7 @@ abstract class Model implements \Serializable
         // Serialization allowed for this model?
         if (!$this->serializable()) {
             throw new FluentModelException(
-                sprintf('Cannot serialize "%s", constant SERIALIZABLE must be set to bool(TRUE)', $this->name)
+                sprintf('Cannot serialize "%s", constant SERIALIZABLE must be set to bool(TRUE)', $this->_name)
             );
         }
 
@@ -155,10 +155,10 @@ abstract class Model implements \Serializable
 
         // Manually declare properties of this (abstract Model) class
         $fluent = [];
-        $fluent["name"] = $this->name;
-        $fluent["table"] = $this->table->_name;
-        $fluent["privateProps"] = $this->privateProps;
-        $fluent["original"] = $this->original;
+        $fluent["name"] = $this->_name;
+        $fluent["table"] = $this->_table->_name;
+        $fluent["privateProps"] = $this->_privateProps;
+        $fluent["original"] = $this->_original;
         $fluent["primaryColumn"] = null;
 
         return serialize(["props" => $props, "fluent" => $fluent]);
@@ -175,7 +175,7 @@ abstract class Model implements \Serializable
         $fluent = $data["fluent"] ?? null;
         if (!is_array($props) || !is_array($fluent)) {
             throw new FluentModelException(
-                sprintf('Cannot retrieve "%s" instance, serialized string may be corrupted', $this->name)
+                sprintf('Cannot retrieve "%s" instance, serialized string may be corrupted', $this->_name)
             );
         }
 
@@ -197,7 +197,7 @@ abstract class Model implements \Serializable
 
         // Bootstrap
         /** @noinspection PhpStrictTypeCheckingInspection */
-        $this->loadTable($this->table);
+        $this->loadTable($this->_table);
 
         // Callback event: onWakeup
         if (method_exists($this, "onWakeup")) {
@@ -212,7 +212,7 @@ abstract class Model implements \Serializable
     final private function populate(array $row): void
     {
         // Make sure we have all the keys present
-        $columnsKeys = $this->table->columns()->names();
+        $columnsKeys = $this->_table->columns()->names();
         foreach ($columnsKeys as $columnsKey) {
             if (!array_key_exists($columnsKey, $row)) {
                 throw new FluentModelException(sprintf('Missing column "%s" in input row', $columnsKey));
@@ -224,7 +224,7 @@ abstract class Model implements \Serializable
             // Ignore undefined columns
             try {
                 /** @var AbstractColumn $column */
-                $column = $this->table->columns()->get($key);
+                $column = $this->_table->columns()->get($key);
             } catch (FluentTableException $e) {
             }
 
@@ -245,7 +245,7 @@ abstract class Model implements \Serializable
                 // Set property
                 $this->set(Comely::camelCase($key), $value);
                 // Preserve original
-                $this->original[$key] = $value;
+                $this->_original[$key] = $value;
             }
         }
     }
@@ -255,7 +255,7 @@ abstract class Model implements \Serializable
      */
     final public function name(): string
     {
-        return $this->name;
+        return $this->_name;
     }
 
     /**
@@ -263,7 +263,7 @@ abstract class Model implements \Serializable
      */
     final public function table(): Table
     {
-        return $this->table;
+        return $this->_table;
     }
 
     /**
@@ -279,10 +279,10 @@ abstract class Model implements \Serializable
 
         // Check if property exists with get_called_class() instead of $this instance for only public props.
         // $this->name = get_called_class()
-        if (property_exists($this->name, $prop)) {
+        if (property_exists($this->_name, $prop)) {
             $this->$prop = $value; // Public property
         } else {
-            $this->privateProps[$prop] = $value; // Private
+            $this->_privateProps[$prop] = $value; // Private
         }
     }
 
@@ -314,14 +314,14 @@ abstract class Model implements \Serializable
     {
         if (is_null($value)) {
             if (!isset($column->_attrs["nullable"])) {
-                throw FluentModelException::BadValue($this->name, $key, 'cannot be NULL');
+                throw FluentModelException::BadValue($this->_name, $key, 'cannot be NULL');
             }
         } else {
             // Compare scalar type
             $valueType = gettype($value);
             if ($valueType !== $column->_scalar) {
                 throw FluentModelException::BadValue(
-                    $this->name,
+                    $this->_name,
                     $key,
                     sprintf('must be of type "%s", given "%s"', $column->_scalar, $valueType)
                 );
@@ -334,7 +334,7 @@ abstract class Model implements \Serializable
      */
     final public function original(): array
     {
-        return $this->original;
+        return $this->_original;
     }
 
     /**
@@ -353,7 +353,7 @@ abstract class Model implements \Serializable
     final public function difference(): array
     {
         $difference = [];
-        foreach ($this->table->columns() as $column) {
+        foreach ($this->_table->columns() as $column) {
             $camelKey = Comely::camelCase($column->_name);
             $existingValue = $this->$camelKey ?? $this->privateProps[$camelKey] ?? null;
             $originalValue = $this->original[$column->_name] ?? null;
@@ -381,27 +381,27 @@ abstract class Model implements \Serializable
      */
     final public function getPrimaryColumn(): ?AbstractColumn
     {
-        if ($this->primaryColumn) {
-            return $this->primaryColumn;
+        if ($this->_primaryColumn) {
+            return $this->_primaryColumn;
         }
 
         // grab Columns
-        $columns = $this->table->columns();
+        $columns = $this->_table->columns();
 
         // Table has PRIMARY KEY defined?
         if ($columns->_primary) {
             try {
-                $this->primaryColumn = $columns->get($columns->_primary);
-                return $this->primaryColumn;
+                $this->_primaryColumn = $columns->get($columns->_primary);
+                return $this->_primaryColumn;
             } catch (FluentTableException $e) {
             }
         }
 
         // Look for first UNIQUE KEY
-        foreach ($this->table->columns() as $column) {
+        foreach ($this->_table->columns() as $column) {
             if (isset($column->_attrs["unique"])) {
-                $this->primaryColumn = $column;
-                return $this->primaryColumn;
+                $this->_primaryColumn = $column;
+                return $this->_primaryColumn;
             }
         }
 
